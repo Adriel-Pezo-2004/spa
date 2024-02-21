bbafbcb03fd4bd52cabfe96307c19695e80b1610
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2024-02-21 18:24:20 -0500

    sin date

--- src/services/trs-date/trs-date.repository.ts
+++ src/services/trs-date/trs-date.repository.ts
@@ -7,51 +7,56 @@ import Dated, { DateDocument, DateModelExt } from 'src/schemas/date/date.schema'
   
 
 @Injectable()
-  export class DateRepository{
-    protected readonly logger = new Logger(DateRepository.name);
-    private readonly DateModel: DateModelExt<DateDocument>
-    
+export class DateRepository extends AbstractRepository<
+    DateDocument,
+  DateAttributes
+> {
+  protected readonly logger = new Logger(DateRepository.name);
 
-
-    async updateOneWithQuery(
-      query: Record<string, any>,
-      update: Partial<DateAttributes>,
-    ): Promise<UpdateWriteOpResult> {
-      try {
-        return await this.DateModel.updateOne(query, update);
-      } catch (error) {
-        // Puedes manejar el error de manera específica si es necesario
-        throw error;
-      }
-    }
-  
-    async getDocumentByCode(code: string): Promise<DateAttributes> {
-      return this.DateModel.findOne({ code }).lean().exec();
-    }
-    async createGenId(data: DateAttributes): Promise<DateAttributes> {
-      const dataToCreate = new this.DateModel({
-        name: data.name,
-        telefono: data.telefono,
-        servicio: data.servicio,
-        separated: data.separated,
-        code: data.code,
-      });
-      return await this.createGenId(dataToCreate);
-    }
-    async getLastCode(): Promise<string> {
-      try {
-        const lastDocument = await this.DateModel
-          .find({})
-          .sort({ code: -1 })
-          .limit(1);
-  
-        const lastCode =
-          lastDocument.length > 0 ? lastDocument[0].code : '01-0000000000';
-  
-        return lastCode;
-      } catch (error) {
-        console.error(error);  // Manejar el error según sea necesario
-        return '01-0000000000'; // O proporcionar un valor predeterminado en caso de error
-      }
-    }
+  constructor(
+    @InjectModel(Dated.name)
+    private readonly DateModel: DateModelExt<DateDocument>,
+    @InjectConnection() connection: Connection,
+  ) {
+    super(DateModel, connection);
+  }
+  async createGenId(data: DateAttributes): Promise<DateAttributes> {
+    const dataToCreate = new this.DateModel({
+      name: data.name,
+      telefono: data.telefono,
+      servicio: data.servicio,
+      separated: data.separated,
+      code: data.code,
+    });
+    return await super.createGenId(dataToCreate);
+  }
+  async getLastCode(): Promise<string> {
+    const lastDocument = await this.DateModel.aggregate<DateAttributes>([
+      {
+        $project: {
+          code: { $ifNull: ['$code', '000000001-0000000000'] },
+          secondNumber: {
+            $cond: [
+              { $eq: [{ $substr: ['$code', 10, 10] }, ''] },
+              0,
+              { $toInt: { $substr: ['$code', 10, 10] } },
+            ],
+          },
+        },
+      },
+      {
+        $sort: {
+          secondNumber: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ])
+      .allowDiskUse(true)
+      .read('secondaryPreferred');
+    const lasCode =
+      lastDocument.length > 0 ? lastDocument[0].code : '000000001-0000000000';
+    return lasCode;
+  }
 }
